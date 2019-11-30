258a56b8932dfba415d17116704c91cf8b8b64db
Anastasios Grammenos <avgramme@ece.auth.gr>
2019-11-30 15:17:30 +0200

    Update elearning tester

--- source/tester.c
+++ source/tester.c
@@ -19,32 +19,32 @@
 int main()
 {
 
-	int n = 897;                    // corpus
-	int m = 762;                    // query 
-	int d = 7;                      // dimensions
-	int k = 13;                     // # neighbors
+  int n=897;                    // corpus
+  int m=762;                    // query
+  int d=7;                      // dimensions
+  int k=13;                     // # neighbors
 
-	double  * corpus = (double *)malloc(n*d * sizeof(double));
-	double  * query = (double *)malloc(m*d * sizeof(double));
+  double  * corpus = (double * ) malloc( n*d * sizeof(double) );
+  double  * query  = (double * ) malloc( m*d * sizeof(double) );
 
-	for (int i = 0; i < n*d; i++)
-		corpus[i] = ((double)(rand())) / (double)RAND_MAX;
+  for (int i=0;i<n*d;i++)
+    corpus[i] = ( (double) (rand()) ) / (double) RAND_MAX;
 
-	for (int i = 0; i < m*d; i++)
-		query[i] = ((double)(rand())) / (double)RAND_MAX;
+  for (int i=0;i<m*d;i++)
+    query[i]  = ( (double) (rand()) ) / (double) RAND_MAX;
 
-	knnresult knnres = kNN(corpus, query, n, m, d, k);
+  knnresult knnres = kNN( corpus, query, n, m, d, k );
 
-	int isValidC = validateResultColMajor(knnres, corpus, query, n, m, d, k);
+  int isValidC = validateResult( knnres, corpus, query, n, m, d, k, COLMAJOR );
 
-	int isValidR = validateResultRowMajor(knnres, corpus, query, n, m, d, k);
+  int isValidR = validateResult( knnres, corpus, query, n, m, d, k, ROWMAJOR );
 
-	printf("Tester validation: %s NEIGHBORS\n",
-		STR_CORRECT_WRONG[isValidC || isValidR]);
+  printf("Tester validation: %s NEIGHBORS\n",
+         STR_CORRECT_WRONG[isValidC||isValidR]);
 
-	free(corpus);
-	free(query);
+  free( corpus );
+  free( query );
 
-	return 0;
+  return 0;
 
 }
--- source/tester_helper.h
+++ source/tester_helper.h
@@ -1,38 +1,74 @@
 #ifndef TESTER_HELPER_H
 #define TESTER_HELPER_H
 
-/* #define VERBOSE */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include "knnring.h"
 
-static char * STR_CORRECT_WRONG[] = { "WRONG", "CORRECT" };
+// ============================== ACCESS MACROS
+
+// ~~~~~~~~~~~~~~~~~~~~ COL-MAJOR
+
+#define COLMAJOR 0
+#define idx_cm(i,j,m,n) (i) + (j)*(m)
+
+#define corpus_cm(i,j)         corpus          [idx_cm(i,j,n,d)               ]
+#define corpusAll_cm(i,j)      corpusAll       [idx_cm(i,j,n*p,d)             ]
+#define knnresnidx_cm(i,j)     knnres.nidx     [idx_cm(i,j,knnres.m,knnres.k) ]
+#define knnresndist_cm(i,j)    knnres.ndist    [idx_cm(i,j,knnres.m,knnres.k) ]
+#define knnresallnidx_cm(i,j)  knnresall.nidx  [idx_cm(i,j,n*p,k)             ]
+#define knnresallndist_cm(i,j) knnresall.ndist [idx_cm(i,j,n*p,k)             ]
+
+#define X_cm(i,j) X[idx_cm(i,j,n,d)]
+#define Y_cm(i,j) Y[idx_cm(i,j,m,d)]
+
+// ~~~~~~~~~~~~~~~~~~~~ ROW-MAJOR
+
+#define ROWMAJOR 1
+#define idx_rm(i,j,m,n) (j) + (i)*(n)
+
+#define corpus_rm(i,j)         corpus          [idx_rm(i,j,n,d)               ]
+#define corpusAll_rm(i,j)      corpusAll       [idx_rm(i,j,n*p,d)             ]
+#define knnresnidx_rm(i,j)     knnres.nidx     [idx_rm(i,j,knnres.m,knnres.k) ]
+#define knnresndist_rm(i,j)    knnres.ndist    [idx_rm(i,j,knnres.m,knnres.k) ]
+#define knnresallnidx_rm(i,j)  knnresall.nidx  [idx_rm(i,j,n*p,k)             ]
+#define knnresallndist_rm(i,j) knnresall.ndist [idx_rm(i,j,n*p,k)             ]
+
+#define X_rm(i,j) X[idx_rm(i,j,n,d)]
+#define Y_rm(i,j) Y[idx_rm(i,j,m,d)]
+
+static char * STR_CORRECT_WRONG[] = {"WRONG", "CORRECT"};
 
 // =================
 // === UTILITIES ===
 // =================
 
-double distColMajor(double *X, double *Y,
-	int i, int j,
-	int d, int n, int m) {
+double distColMajor(double const * const X, double const * const Y,
+                    int i, int j,
+                    int d, int n, int m){
 
-	/* compute distance */
-	double dist = 0;
-	for (int l = 0; l < d; l++) {
-		dist += (X[l*n + i] - Y[l*m + j]) * (X[l*n + i] - Y[l*m + j]);
-	}
+  /* compute distance */
+  double dist = 0;
+  for (int l = 0; l < d; l++){
+    dist += ( X_cm(i,l) - Y_cm(j,l) ) * ( X_cm(i,l) - Y_cm(j,l) );
+  }
 
-	return sqrt(dist);
+  return sqrt(dist);
 }
 
-double distRowMajor(double *X, double *Y,
-	int i, int j,
-	int d, int n, int m) {
+double distRowMajor(double const * const X, double const * const Y,
+                    int i, int j,
+                    int d, int n, int m){
 
-	/* compute distance */
-	double dist = 0;
-	for (int l = 0; l < d; l++) {
-		dist += (X[l + i * d] - Y[l + j * d]) * (X[l + i * d] - Y[l + j * d]);
-	}
+  /* compute distance */
+  double dist = 0;
+  for (int l = 0; l < d; l++){
+    dist += ( X_rm(i,l) - Y_rm(j,l) ) * ( X_rm(i,l) - Y_rm(j,l) );
+  }
 
-	return sqrt(dist);
+  return sqrt(dist);
 }
 
 
@@ -41,145 +77,100 @@ double distRowMajor(double *X, double *Y,
 // === VALIDATION ===
 // ==================
 
-//! kNN validator (col major)
+//! kNN validator
 /*!
    The function asserts correctness of the kNN results by:
-	 (i)   Checking that reported distances are correct
-	 (ii)  Validating that distances are sorted in non-decreasing order
-	 (iii) Ensuring there are no other points closer than the kth neighbor
+     (i)   Checking that reported distances are correct
+     (ii)  Validating that distances are sorted in non-decreasing order
+     (iii) Ensuring there are no other points closer than the kth neighbor
 */
-int validateResultColMajor(knnresult knnres,
-	double * corpus, double * query,
-	int n, int m, int d, int k) {
+int validateResult( knnresult knnres,
+                    double const * const corpus, double const * const query,
+                    int n, int m, int d, int k, int ap ) {
 
-	/* loop through all query points */
-	for (int j = 0; j < m; j++) {
+  /* loop through all query points */
+  for (int j = 0; j < m; j++ ){
 
-		/* max distance so far (equal to kth neighbor after nested loop) */
-		double maxDist = -1;
+    /* max distance so far (equal to kth neighbor after nested loop) */
+    double maxDist = -1;
 
-		/* mark all distances as not computed */
-		int * visited = (int *)calloc(n, sizeof(int));
+    /* mark all distances as not computed */
+    int * visited = (int *) calloc( n, sizeof(int) );
 
-		/* go over reported k nearest neighbors */
-		for (int i = 0; i < k; i++) {
+    /* go over reported k nearest neighbors */
+    for (int l = 0; l < k; l++ ){
 
-			/* keep list of visited neighbors */
-			visited[knnres.nidx[i*m + j]] = 1;
+      if (ap == COLMAJOR){
 
-			/* get distance to stored index */
-			double distxy = distColMajor(corpus, query, knnres.nidx[i*m + j], j, d, n, m);
+        /* keep list of visited neighbors */
+        visited[ knnresnidx_cm(j,l) ] = 1;
 
-			/* make sure reported distance is correct */
-			if (fabs(knnres.ndist[i*m + j] - distxy) > 1e-8) return 0;
+        /* get distance to stored index */
+        double distxy = distColMajor( corpus, query, knnresnidx_cm(j,l), j, d, n, m );
 
-			/* distances should be non-decreasing */
-			if (knnres.ndist[i*m + j] < maxDist) return 0;
+        /* make sure reported distance is correct */
+        if ( fabs( knnresndist_cm(j,l) - distxy ) > 1e-6 ) return 0;
 
-			/* update max neighbor distance */
-			maxDist = knnres.ndist[i*m + j];
+        /* distances should be non-decreasing */
+        if ( knnresndist_cm(j,l) < maxDist ) return 0;
 
-		} /* for (k) -- reported nearest neighbors */
+        /* update max neighbor distance */
+        maxDist = knnresndist_cm(j,l);
 
-		/* now maxDist should have distance to kth neighbor */
+      } else {
 
-		/* check all un-visited points */
-		for (int i = 0; i < n; i++) {
+        /* keep list of visited neighbors */
+        visited[ knnresnidx_rm(j,l) ] = 1;
 
-			/* check only (n-k) non-visited nodes */
-			if (!visited[i]) {
+        /* get distance to stored index */
+        double distxy = distRowMajor( corpus, query, knnresnidx_rm(j,l), j, d, n, m );
 
-				/* get distance to unvisited vertex */
-				double distxy = distColMajor(corpus, query, i, j, d, n, m);
+        /* make sure reported distance is correct */
+        if ( fabs( knnresndist_rm(j,l) - distxy ) > 1e-6 ) return 0;
 
-				/* point cannot be closer than kth distance */
-				if (distxy < maxDist) return 0;
+        /* distances should be non-decreasing */
+        if ( knnresndist_rm(j,l) < maxDist ) return 0;
 
-			} /* if (!visited[i]) */
+        /* update max neighbor distance */
+        maxDist = knnresndist_rm(j,l);
 
-		} /* for (i) -- unvisited notes */
 
-		/* deallocate memory */
-		free(visited);
+      }
 
-	} /* for (j) -- query points */
+    } /* for (k) -- reported nearest neighbors */
 
-	/* return */
-	return 1;
+    /* now maxDist should have distance to kth neighbor */
+
+    /* check all un-visited points */
+    for (int i = 0; i < n; i++ ){
+
+      /* check only (n-k) non-visited nodes */
+      if (!visited[i]){
+
+        double distxy;
+
+        /* get distance to unvisited vertex */
+        if (ap == COLMAJOR)
+          distxy = distColMajor( corpus, query, i, j, d, n, m );
+        else
+          distxy = distRowMajor( corpus, query, i, j, d, n, m );
+
+        /* point cannot be closer than kth distance */
+        if ( distxy < maxDist ) return 0;
+
+      } /* if (!visited[i]) */
+
+    } /* for (i) -- unvisited notes */
+
+    /* deallocate memory */
+    free( visited );
+
+  } /* for (j) -- query points */
+
+  /* return */
+  return 1;
 
 }
-
-
-//! kNN validator (row major)
-/*!
-   The function asserts correctness of the kNN results by:
-	 (i)   Checking that reported distances are correct
-	 (ii)  Validating that distances are sorted in non-decreasing order
-	 (iii) Ensuring there are no other points closer than the kth neighbor
-*/
-int validateResultRowMajor(knnresult knnres,
-	double * corpus, double * query,
-	int n, int m, int d, int k) {
-
-	/* loop through all query points */
-	for (int j = 0; j < m; j++) {
-
-		/* max distance so far (equal to kth neighbor after nested loop) */
-		double maxDist = -1;
-
-		/* mark all distances as not computed */
-		int * visited = (int *)calloc(n, sizeof(int));
-
-		/* go over reported k nearest neighbors */
-		for (int i = 0; i < k; i++) {
-
-			/* keep list of visited neighbors */
-			visited[knnres.nidx[i + j * k]] = 1;
-
-			/* get distance to stored index */
-			double distxy = distRowMajor(corpus, query, knnres.nidx[i + j * k], j, d, n, m);
-
-			/* make sure reported distance is correct */
-			if (fabs(knnres.ndist[i + j * k] - distxy) > 1e-8) return 0;
-
-			/* distances should be non-decreasing */
-			if (knnres.ndist[i + j * k] < maxDist) return 0;
-
-			/* update max neighbor distance */
-			maxDist = knnres.ndist[i + j * k];
-
-		} /* for (k) -- reported nearest neighbors */
-
-		/* now maxDist should have distance to kth neighbor */
-
-		/* check all un-visited points */
-		for (int i = 0; i < n; i++) {
-
-			/* check only (n-k) non-visited nodes */
-			if (!visited[i]) {
-
-				/* get distance to unvisited vertex */
-				double distxy = distRowMajor(corpus, query, i, j, d, n, m);
-
-				/* point cannot be closer than kth distance */
-				if (distxy < maxDist) return 0;
-
-			} /* if (!visited[i]) */
-
-		} /* for (i) -- unvisited notes */
-
-		/* deallocate memory */
-		free(visited);
-
-	} /* for (j) -- query points */
-
-	/* return */
-	return 1;
-
-}
-
-
-
 
 
 #endif
