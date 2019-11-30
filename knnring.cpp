0da93ef7c507ad799c8cc5175816cea2ec1f92b1
Anastasios Grammenos <avgramme@ece.auth.gr>
2019-11-30 16:01:48 +0200

    Fix bugs in main_sequential

--- knnring.cpp
+++ knnring.cpp
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include "cblas.h"
+#include "mpi.h"
 
 
 void blas_distXY(double *D, double *X, double *Y, int n, int m, int d)
@@ -66,33 +68,109 @@ void my_qsort(double *d, int *idx, int len, int k)
 		quick_select(d, idx, k + 1, i);
 }
 
-knnresult kNN(double *X, double  *Y, int n, int m, int d, int k)
+void add_idx_offset(int offset, int *idx, int n, int k)
 {
-	knnresult result;
-	result.nidx = (int*)malloc(m * k * sizeof(int));
-	result.ndist = (double*)malloc(m * k * sizeof(double));
-	result.m = m;
-	result.k = k;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < k; j++)
+			*(idx + i * k + j) += offset;
+}
 
+void kNNv(knnresult *result, double *X, double  *Y, int n, int m, int d, int k)
+{
 	double *distxy = (double*)calloc(m*n, sizeof(double));
 	int *idx = (int*)malloc(n * sizeof(int));
 	blas_distXY(distxy, X, Y, n, m, d); // find all distances
 	// Find k neighbors for every query
-	for (int q = 0; q < m; q++) {	// all query
+	for (int q = 0; q < m; q++) {	// all query		
 		for (int c = 0; c < n; c++)	// create index array
 			*(idx + c) = c;
 		my_qsort((distxy + q * n), idx, n, k);
 		for (int z = 0; z < k; z++) {	// add to struct the k neighbors
-			*(result.nidx + q * k + z) = *(idx + z);
-			*(result.ndist + q * k + z) = *(distxy + q * n + z);
+			*(result->nidx + q * k + z) = *(idx + z);
+			*(result->ndist + q * k + z) = *(distxy + q * n + z);
 		}
 	}
 	free(idx);
 	free(distxy);
-	return result;
 }
 
-knnresult distrAllkNN(double *X, int n, int d, int k)
+knnresult distrAllkNN(double *Y, int n, int d, int k)
 {
-	return kNN(X, X, n, n, d, k);
+	int p, id;							// # processess and PID
+	MPI_Comm_rank(MPI_COMM_WORLD, &id); // Task ID
+	MPI_Comm_size(MPI_COMM_WORLD, &p);	// # tasks
+
+	// Where receive? Where send?
+	int source = (id == 0) ? p - 1 : id - 1;	// if id == 0 take from last proccess
+	int dest = (id == p - 1) ? 0 : id + 1;		// if last proccess send to first proccess
+	int tag = (int)'X';							// mpi tag to send X data points set
+	const int num_of_requests = 2;				// mpi asychronous # of status for request
+	MPI_Status Stat[num_of_requests];			// mpi asychronous receive status	
+	MPI_Request mpi_requests[num_of_requests];	// mpi asychronou request status
+
+	// Allocate memory for the jobs
+	int *npidx = (int*)malloc(n * (2 * k) * sizeof(int));
+	double *npdist = (double*)malloc(n * (2 * k) * sizeof(double));
+	double *X = (double*)malloc(n * d * sizeof(double));
+	memcpy(X, Y, n * d * sizeof(double));		// your chunk are the first points to check
+	double *X_recv = (double*)malloc(n * d * sizeof(double));
+	knnresult new_knnresult;
+	new_knnresult.m = n;
+	new_knnresult.k = k;
+	new_knnresult.nidx = (int*)malloc(n * k * sizeof(int));
+	new_knnresult.ndist = (double*)malloc(n * k * sizeof(double));
+
+	// Move points into circle
+	for (int ip = 0; ip < p; ip++) {
+		if (ip < p - 1) {	// don't send data at the last loop, the job was done at the first
+			// Send and receive data asychronous from other proccess in the ring
+			MPI_Isend(X, n*d, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD, &mpi_requests[0]);
+			MPI_Irecv(X_recv, n*d, MPI_DOUBLE, source, tag, MPI_COMM_WORLD, &mpi_requests[1]);
+
+		}
+		if (ip == 0) {	// at first loop you already have data			
+			kNNv(&new_knnresult, X, Y, n, n, d, k);
+			int offset = (id >= ip + 1) ? (id - ip - 1) * n : (p + id - ip - 1) * n;
+			add_idx_offset(offset, new_knnresult.nidx, n, k);				// offset idx depending of the chunk
+			for (int i = 0; i < n; i++) {
+				memcpy((npidx + i * (2 * k)), (new_knnresult.nidx + i * k), k * sizeof(int));
+				memcpy((npdist + i * (2 * k)), (new_knnresult.ndist + i * k), k * sizeof(double));
+			}
+		}
+		else {	// work with received data
+			kNNv(&new_knnresult, X, Y, n, n, d, k);
+			int offset = (id >= ip + 1) ? (id - ip - 1) * n : (p + id - ip - 1) * n;
+			add_idx_offset(offset, new_knnresult.nidx, n, k);				// offset idx depending of the chunk
+			for (int i = 0; i < n; i++) {
+				memcpy((npidx + i * (2 * k) + k), (new_knnresult.nidx + i * k), k * sizeof(int));
+				memcpy((npdist + i * (2 * k) + k), (new_knnresult.ndist + i * k), k * sizeof(double));
+				my_qsort((npdist + i * (2 * k)), (npidx + i * (2 * k)), 2 * k, k);
+			}
+		}
+		if (ip < p - 1) {
+			MPI_Waitall(num_of_requests, mpi_requests, Stat);	// wait Isent and Irecv
+			// Swap pointers
+			double *tmpdptr = X;
+			X = X_recv;
+			X_recv = tmpdptr;
+		}
+	}
+
+	free(X_recv);
+	free(X);
+
+	knnresult final_knnresult;
+	final_knnresult.nidx = (int*)malloc(n * k * sizeof(int));
+	final_knnresult.ndist = (double*)malloc(n * k * sizeof(double));
+	for (int i = 0; i < n; i++) {	// copy results to return structure
+		memcpy((final_knnresult.nidx + i * k), (npidx + i * (2 * k)), k * sizeof(int));
+		memcpy((final_knnresult.ndist + i * k), (npdist + i * (2 * k)), k * sizeof(double));
+	}
+	final_knnresult.m = n;
+	final_knnresult.k = k;
+
+	free(npidx);
+	free(npdist);
+
+	return final_knnresult;
 }
--- source/knnring/src/main_sequential.c
+++ source/knnring/src/main_sequential.c
@@ -13,6 +13,7 @@
 #define DefaultNumNeighbors 13
 #define MatrixOrder ROWMAJOR	// COLMAJOR | ROWMAJOR | 2 = Check both
 #define DefaultValidateResults 1
+#define DefaultSelf 0	// use as query the corpus
 
 int n = DefaultNumCorpusPoints;
 int m = DefaultNumQueryPoints;
@@ -20,6 +21,7 @@ int d = DefaultDim;
 int k = DefaultNumNeighbors;
 int ap = MatrixOrder;
 int vr = DefaultValidateResults;
+int self_q = DefaultSelf;
 
 struct timeval startwtime, endwtime;
 
@@ -36,35 +38,44 @@ int main(int argc, char *argv[])
 	printf("Generating random data set. ");
 	gettimeofday(&startwtime, NULL);
 	double *corpus = (double *)malloc(n * d * sizeof(double));
-	double *query = (double *)malloc(m * d * sizeof(double));
+	double *query = corpus;
 	for (int i = 0; i < n*d; i++)
 		*(corpus + i) = ((double)rand() / (RAND_MAX));
-	for (int i = 0; i < m*d; i++)
-		*(query + i) = ((double)rand() / (RAND_MAX));
+	if (!self_q) {
+		query = (double *)malloc(m * d * sizeof(double));
+		for (int i = 0; i < m*d; i++)
+			*(query + i) = ((double)rand() / (RAND_MAX));
+	}
 	gettimeofday(&endwtime, NULL);
 	double p_time = (double)((endwtime.tv_usec - startwtime.tv_usec) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
 	printf("DONE in %fsec!\n", p_time);
 
 	// Find neighbors
 	printf("Finding all %i neighbors for all %i query points. ", k, m);
-	gettimeofday(&startwtime, NULL);
-	knnresult knnres = kNN(corpus, query, n, m, d, k);
+	knnresult knnres;
+	gettimeofday(&startwtime, NULL);	
+	if (!self_q)
+		knnres = kNN(corpus, query, n, m, d, k);
+	else
+		knnres = distrAllkNN(corpus, n, d, k);
 	gettimeofday(&endwtime, NULL);
 	p_time = (double)((endwtime.tv_usec - startwtime.tv_usec) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
 	printf("DONE in %fsec!\n", p_time);
 
-	// Validate results
-	printf("Validating results. ");
-	gettimeofday(&startwtime, NULL);
-	int isValid = (MatrixOrder == 2) ? validateResult(knnres, corpus, query, n, m, d, k, COLMAJOR) || validateResult(knnres, corpus, query, n, m, d, k, ROWMAJOR)
-		: validateResult(knnres, corpus, query, n, m, d, k, ap);
-	printf("Tester validation: %s NEIGHBORS. ", STR_CORRECT_WRONG[isValid]);
-	gettimeofday(&endwtime, NULL);
-	p_time = (double)((endwtime.tv_usec - startwtime.tv_usec) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
-	printf("DONE in %fsec!\n", p_time);
-
+	if (vr) {
+		// Validate results
+		printf("Validating results. ");
+		gettimeofday(&startwtime, NULL);
+		int isValid = (MatrixOrder == 2) ? validateResult(knnres, corpus, query, n, m, d, k, COLMAJOR) || validateResult(knnres, corpus, query, n, m, d, k, ROWMAJOR)
+			: validateResult(knnres, corpus, query, n, m, d, k, ap);
+		printf("Tester validation: %s NEIGHBORS. ", STR_CORRECT_WRONG[isValid]);
+		gettimeofday(&endwtime, NULL);
+		p_time = (double)((endwtime.tv_usec - startwtime.tv_usec) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
+		printf("DONE in %fsec!\n", p_time);
+	}
 	printf("Exiting\n");
-	free(query);
+	if (!self_q)
+		free(query);
 	free(corpus);
 	return 0;
 }
@@ -85,8 +96,11 @@ void help(int argc, char *argv[])
 					k = atoi(argv[i + 1]);
 				else if (*(argv[i] + 1) == 'o')
 					ap = atoi(argv[i + 1]);
-				else if (*(argv[i] + 1) == 'v') {
+				else if (*(argv[i] + 1) == 'v')
 					vr = atoi(argv[i + 1]);
+				else if (*(argv[i] + 1) == 's') {
+					self_q = atoi(argv[i + 1]);
+					m = n;
 				}
 				else {
 					help(1, argv);
@@ -107,4 +121,5 @@ void help(int argc, char *argv[])
 	printf("-d [Dimension]\t:Dimension of the space (default: %i)\n", DefaultDim);
 	printf("-o [0|1|2]\t:Matrix order with 0=ColMajor|1=RowMajor|2=Check both (default:%i)\n", ROWMAJOR);
 	printf("-v [0|1]\t:Validate results with elearning tester (default:%i)\n", DefaultValidateResults);
+	printf("-s [0|1]\t:Use as query the corpus (default:%i)\n", DefaultSelf);
 }
